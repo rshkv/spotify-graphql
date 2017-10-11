2fc0beb7da5aad3805dcd8521fe9ca39ede48e66
Willi Raschkowski <willi@raschkowski.com>
2017-10-11 18:19:54 +0200

    Client credentials flow

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
 import * as express from "express";
 import * as graphqlHTTP from "express-graphql";
+import * as SpotifyWebApi from "spotify-web-api-node";
+import { clientId, clientSecret } from "./apiConfig";
 import schema from "./schema";
 
 const PORT = 4000;
 const app = express();
+const spotifyApi = new SpotifyWebApi({
+    clientId,
+    clientSecret,
+    redirectUri: `https://localhost:${PORT}/login`,
+});
 
-app.use("/graphql", graphqlHTTP({
-    graphiql: true,
-    schema,
-}));
+const startServer = async () => {
+    // Retrieve and set access token
+    const accessToken = (await spotifyApi.clientCredentialsGrant())
+        .body
+        .access_token;
+    spotifyApi.setAccessToken(accessToken);
 
-app.listen(PORT, () => {
+    // Setup graphql handler
+    app.use("/graphql", graphqlHTTP({
+        context: { spotifyApi },
+        graphiql: true,
+        schema,
+    }));
+
+    app.listen(PORT, () => {
+        // tslint:disable-next-line no-console
+        console.log(`Spotify GraphQL server running on port ${PORT}.`);
+    });
+};
+
+startServer().catch((e) => {
     // tslint:disable-next-line no-console
-    console.log(`Spotify GraphQL server running on port ${PORT}.`);
+    console.error(e);
 });
