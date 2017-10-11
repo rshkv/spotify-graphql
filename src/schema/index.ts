96a3e2184d53722e0e4834845773f7cb792690e0
Willi Raschkowski <willi@raschkowski.com>
2017-10-12 01:35:37 +0200

    Query artists and playlists

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,9 +2,140 @@ import { makeExecutableSchema } from "graphql-tools";
 import resolvers from "./resolvers";
 
 const typeDefs = `
-    type Query {
-        hello: String!
-    }
+	type Query {
+		search(q: String!, type: [SearchType!]): [String!]
+		artist(id: String!): ArtistFull
+		playlist(user_id: String!, playlist_id: String!): PlaylistFull!
+	}
+
+	type AlbumSimplified {
+		# The type of the album: one of "album", "single", or "compilation".
+		album_type: String!
+		artists: [ArtistSimplified!]!
+		available_markets: [String!]!
+		external_urls: ExternalUrl!
+		href: String!
+		id: String!
+		images: [Image!]!
+		name: String!
+		type: String!
+		uri: String!
+	}
+
+	type ArtistSimplified {
+		name: String!
+	}
+
+	type ArtistFull {
+		external_urls: ExternalUrl!
+		followers: Followers
+		genres: [String!]!
+		href: String!
+		id: String!
+		images: [Image!]!
+		name: String!
+		popularity: Int!
+		type: String!
+		uri: String!
+	}
+
+	type ExternalUrl {
+		spotify: String
+	}
+
+	type ExternalId {
+		isrc: String
+		ean: String
+		upc: String
+	}
+
+	type Followers {
+		href: String
+		total: Int
+	}
+
+	type Image {
+		height: Int
+		url: String!
+		width: Int
+	}
+
+	type UserPublic {
+		display_name: String!
+		external_urls: ExternalUrl!
+		href: String!
+		id: String!
+		type: String!
+		uri: String!
+	}
+
+	type PlaylistFull {
+		collaborative: Boolean!
+		description: String
+		external_urls: ExternalUrl!
+		followers: Followers!
+		href: String!
+		id: String!
+		images: [Image!]!
+		name: String!
+		owner: UserPublic!
+		public: Boolean
+		snapshot_id: String!
+		tracks(limit: Int, offset: Int): [PlaylistTrack!]!
+		type: String!
+		uri: String!
+	}
+
+	type PlaylistTrack {
+		added_at: String
+		added_by: UserPublic
+		is_local: Boolean!
+		track: TrackFull!
+	}
+
+	type SearchResult {
+		albums: [String]!
+		artists: [String]!
+		playlists: [String]!
+		tracks: [String]!
+	}
+
+	enum SearchType {
+		album
+		artist
+		playlist
+		track
+	}
+
+	type TrackFull {
+		album: AlbumSimplified!
+		artists: [ArtistSimplified!]!
+		available_markets: [String!]!
+		disc_number: Int!
+		duration_ms: Int!
+		explicit: Boolean!
+		external_ids: ExternalId!
+		external_urls: ExternalUrl!
+		href: String!
+		id: String!
+		is_playable: Boolean
+		linked_from: TrackLink
+		name: String!
+		popularity: Int!
+		preview_url: String!
+		track_number: Int!
+		type: String!
+		uri: String!
+	}
+
+	type TrackLink {
+		external_urls: ExternalUrl!
+		href: String!
+		id: String!
+		type: String!
+		uri: String!
+	}
+
 `;
 
 export default makeExecutableSchema({ typeDefs, resolvers });
--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -1,7 +1,43 @@
+import * as _ from "lodash";
+
+function log(o) {
+    // tslint:disable-next-line no-console
+    console.log(JSON.stringify(o, null, 2));
+}
+
 export default {
 
+    PlaylistFull: {
+        tracks: async (root, { limit = 100, offset = 0 }, { spotifyApi }) => {
+            if (offset + limit > 100) {
+                return (await spotifyApi.getPlaylistTracks(root.owner.id, root.id, { limit, offset }))
+                    .body.items;
+            }
+            return _.slice(root.tracks.items, offset, offset + limit);
+        },
+    },
+
     Query: {
-        hello: () => "Hello, world!",
+
+        artist: async (root, { id }, { spotifyApi }) => {
+            return (await spotifyApi.getArtist(id)).body;
+        },
+
+        playlist: async (root, { user_id, playlist_id }, { spotifyApi }) => {
+            const res = (await spotifyApi.getPlaylist(user_id, playlist_id)).body;
+            return res;
+        },
+
+        search: async (root, { q, type }, { spotifyApi }) => {
+            const searchResponse = await spotifyApi.search(q, type, { limit: 1 });
+            const items = _.mapValues(searchResponse.body, (v) => v.items);
+            const searchResult = {
+                albums: [], artists: [], playlists: [], tracks: [],
+                ...items,
+            };
+            return ["this", "is", "a", "test"];
+        },
+
     },
 
 };
